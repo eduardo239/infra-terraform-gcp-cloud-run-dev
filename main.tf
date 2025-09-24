fe02aea6600a192c460824ce0035f029a8358649
eduardo239 <eucrieiumaconta@gmail.com>
2025-09-23 21:11:59 -0300

    Fix

--- main.tf
+++ main.tf
@@ -1,4 +1,11 @@
 
+terraform {
+  backend "gcs" {
+    bucket = "learn-gcp-terraform-469711-terraform-state"
+    prefix = "terraform/state/dev"
+
+  }
+}
 
 provider "google" {
   project = "learn-gcp-terraform-469711"
